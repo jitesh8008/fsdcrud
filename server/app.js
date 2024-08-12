f03c8f9109fd5a2038f027ea7637a6e0c60940f0
jitesh8008 <jitesh8008@gmail.com>
2024-08-12 20:50:37 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// Updated MongoDB connection URL for replica set
+
 const url = 'mongodb://localhost:27020,localhost:27021,localhost:27022/cbititl?replicaSet=m101';
 
 const app = express();
 
-// Connect to MongoDB with the replica set configuration
+
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const con = mongoose.connection;
